36af61024e07433d46c8e451c7136a04b07ad2f8
Adrian <apsaxallday@gmail.com>
2019-03-12 12:21:45 -0500

    Taylor's Results Page for clean build

--- ABET/ABET/ResultsPage.cs
+++ ABET/ABET/ResultsPage.cs
@@ -8,24 +8,52 @@ using Xamarin.Forms;
 namespace ABET
 {
     public class ResultsPage : ContentPage
-    { 
+    {
+        Grid classGrid = new Grid();
+        Grid buttonGrid = new Grid();
+        TableView classTableView;
+        ListView infoListView;
+        TableSection switchCellTable;
+        List<string> semesterList;
+        List<Cell> temporarylist;
+        Picker picker;
+
+
+        //  General string array for temp use
+        string[] terms = new string[]
+        {
+            "Spring 2015",
+            "Fall 2015",
+            "Spring 2016",
+            "Fall 2016",
+            "Spring 2017",
+            "Fall 2017",
+            "Spring 2018",
+            "Fall 2018",
+        };
+        string[] classes = new string[]
+{
+            "Some engineering",
+            "Some engineering",
+            "Some engineering",
+            "Some engineering",
+};
+
         public ResultsPage()
         {
 
             //Create the grid for the page that displays the list of classes and pages.
-            Grid grid = new Grid();
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(.2, GridUnitType.Star) });
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(3, GridUnitType.Star) });
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            classGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(.2, GridUnitType.Star) });
+            classGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(3, GridUnitType.Star) });
+            classGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            classGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+            classGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
             //Create the grid for the Buttons in the lower left panel
-            Grid newGrid = new Grid();
-            newGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            newGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            newGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-            newGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            buttonGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            buttonGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            buttonGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            buttonGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
             //Create the buttons to perform certain function son the information
             //along with some extra placeholder functions while we figure out what
@@ -38,161 +66,132 @@ namespace ABET
             sButton.Text = "Function";
             Button vButton = new Button();
             vButton.Text = "Function";
-            newGrid.Children.Add(rawButton, 0, 0);
-            newGrid.Children.Add(averageButton, 1, 0);
-            newGrid.Children.Add(sButton, 0, 1);
-            newGrid.Children.Add(vButton, 1, 1);
+            buttonGrid.Children.Add(rawButton, 0, 0);
+            buttonGrid.Children.Add(averageButton, 1, 0);
+            buttonGrid.Children.Add(sButton, 0, 1);
+            buttonGrid.Children.Add(vButton, 1, 1);
 
-            //placeholder picker for demo purposes
-            Picker picker = new Picker();
-            List<string> semesterList = new List<string>();
-            semesterList.Add("Fall 2015");
-            semesterList.Add("Spring 2016");
-            semesterList.Add("Fall 2016");
-            semesterList.Add("Spring 2017");
-            semesterList.Add("Fall 2017");
-            semesterList.Add("Spring 2018");
-            semesterList.Add("Fall 2018");
-            picker.ItemsSource = semesterList;
+            //Events handlers when button is clicked
+            //will call corresponding function
+            rawButton.Clicked += OnRawDataClicked;
+            averageButton.Clicked += OnAverageSelected;
+
+
+            picker = new Picker();
+            semesterList = new List<string>();
+            infoListView = new ListView();
+
+
+
+            //TODO
+            //really need these prepared statements
+            //  get the semesters to choose from
+            //      SELECT semester, year FROM semesters
+            //  get the courses for each semester
+            //      SELECT courseTitle, year FROM course, semesters WHERE semester = '___' and year = 
+            //  get the reponses for the courses
+            //      SELECT goal, reponse FROM surveys 
+
+
+            //depending on what sql select statements might have to change this
+            picker.ItemsSource = terms;
             picker.SelectedIndex = 0;
+            picker.SelectedIndexChanged += this.PickerSemester;
 
-            //placeholder list of switches for demo purposes
-            TableView section = new TableView
+
+            //Controls the class picker selection
+            classTableView = new TableView
             {
-                Root = new TableRoot {
-                     new TableSection("Fall 2015") { 
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-                        new SwitchCell () {Text = "Some Engineering Class" },
-
-                     }
-                },
+                Root = new TableRoot { },
                 Intent = TableIntent.Settings
             };
 
-            //placeholder list of data
-            //this will most likely need to change to accomodate the layout of information in the 
-            //the ABET classes.
-            ListView listView = new ListView();
-            listView.ItemsSource = new string[]
+            switchCellTable = new TableSection(picker.SelectedItem.ToString()) { };
+            foreach (var element in classes)
             {
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-                "1",
-            };
+                switchCellTable.Add(new SwitchCell() { Text = element });
+            }
+            classTableView.Root.Add(switchCellTable);
+
 
             //populates top left of results page
-            grid.Children.Add(listView, 0, 0);
-            Grid.SetRowSpan(listView, 2);
+            classGrid.Children.Add(infoListView, 0, 0);
+            Grid.SetRowSpan(infoListView, 2);
 
             //populates bottom left of the results page
-            grid.Children.Add(newGrid, 0, 2);
+            classGrid.Children.Add(buttonGrid, 0, 2);
 
             //populates top right of the result page
-            grid.Children.Add(picker, 1, 0);
+            classGrid.Children.Add(picker, 1, 0);
 
             //populates the bottom right
-            grid.Children.Add(section, 1, 1);
-            Grid.SetRowSpan(section, 2);
+            classGrid.Children.Add(classTableView, 1, 1);
+            Grid.SetRowSpan(classTableView, 2);
 
-            Content = grid;
+            Content = classGrid;
         }
 
 
         //here are all the button, picker, and switch functionalities.
 
         //Changes the list of engineering classes based on the semester chosen from the picker
-        async void OnSemesterSelected (object sender, EventArgs e)
+        public void PickerSemester(object sender, EventArgs e)
         {
             //populate the TableView section with a switchcell representing each 
             //engineering class in the semester selected
+            if (picker.SelectedItem.ToString() == classTableView.Root.ToString())
+            { }
+            else
+            {
+                classGrid.Children.RemoveAt(3);
+                classTableView = new TableView
+                {
+                    Root = new TableRoot { },
+                    Intent = TableIntent.Settings
+                };
+
+                switchCellTable = new TableSection(picker.SelectedItem.ToString()) { };
+                foreach (var element in classes)
+                {
+                    switchCellTable.Add(new SwitchCell() { Text = element + " " + picker.SelectedItem.ToString() });
+                }
+                classTableView.Root.Add(switchCellTable);
+                classGrid.Children.Add(classTableView, 1, 1);
+
+            }
         }
 
         //Performs the Average Function on the selected classes
-        async void OnAverageSelected (object sender, EventArgs e)
+        public void OnAverageSelected(object sender, EventArgs e)
         {
             //Populate the top left panel with the results of the function 
             //performed on the classes (switchcells) selected in the bottom left panel
+            infoListView.ItemsSource = new string[]
+            {
+                "average",
+            };
+
         }
 
         //Outputs the complete responses from a selected class
-        async void OnRawDataSelected (object sender, EventArgs e)
+        public void OnRawDataClicked(object sender, EventArgs e)
         {
             //Populate the top left panel with the results of the function 
             //performed on the classes (switchcells) selected in the bottom left panel
+            temporarylist = switchCellTable.ToList();
+            List<string> temporary = new List<string>();
+            foreach (SwitchCell element in temporarylist)
+            {
+                if (element.On)
+                {
+                    temporary.Add(element.Text.ToString());
+                }
+            }
+            if (temporary.Count == 0)
+            {
+                temporary.Add("empty");
+            }
+            infoListView.ItemsSource = temporary;
         }
     }
 }
